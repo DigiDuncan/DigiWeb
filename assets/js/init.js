a827e192ca2980bcbc238b1691e1ed7679d42911
cubed <49422290+cryptxcubed@users.noreply.github.com>
2019-11-09 18:14:34 -0500

    Add files via upload

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+document.addEventListener("DOMContentLoaded", function() {
+    OverlayScrollbars(document.querySelectorAll('body'), {
+        className  : "os-theme-light", 
+     });
+});
+
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active");
+});
