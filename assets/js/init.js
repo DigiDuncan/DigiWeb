78705f4824c58586ee70a353bc651fe8f41ae49e
cubed <49422290+cryptxcubed@users.noreply.github.com>
2019-11-13 17:28:12 -0500

    Added dark/light mode + toggle.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,38 @@
 AOS.init();document.addEventListener("DOMContentLoaded",function(){OverlayScrollbars(document.querySelectorAll('body'),{className:"os-theme-light",});});var $hamburger=$(".hamburger");$hamburger.on("click",function(e){$hamburger.toggleClass("is-active");});
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+}
+
+
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
