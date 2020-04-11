71b23c3cac428ba289af198d9756caecd58b4200
DigiDuncan <digiduncan@gmail.com>
2020-04-11 14:36:50 -0400

    stupid time test.

--- digiweb/main.py
+++ digiweb/main.py
@@ -1,13 +1,17 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return "Hello, World!"
-    
+    return datetime.datetime.now()
+
+
 def main():
     app.run()
+
 
 if __name__ == "__main__":
     main()
